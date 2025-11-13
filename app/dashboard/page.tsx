c2705a86b9013bfa72f2ac5c35e8f939fb1d6f34
s.cheriyancode <s.cheriyancode@accenture.com>
2025-11-13 15:42:18 +0530

    learn next js

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+interface DashboardProps {
+  title: string;
+}
+const Dashboard = ({ title }: DashboardProps) => {
+  return <div>Dashboardddd</div>;
+};
+
+export default Dashboard;
